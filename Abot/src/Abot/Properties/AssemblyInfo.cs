67d82fad1df5e1f0f59e751b17df4e74de69955e
sjdirect <sjdirect@gmail.com>
2016-10-09 08:12:53 -0700

    Imported existing files and minor cleaning of obsolete and serializable attributes/methods

--- Abot/src/Abot/Properties/AssemblyInfo.cs
+++ Abot/src/Abot/Properties/AssemblyInfo.cs
@@ -5,10 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Abot")]
+[assembly: AssemblyDescription("C# Web Crawler")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Abot")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -17,3 +21,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0aabb4c5-efdd-48ad-af9b-ac3634f5a9d0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
